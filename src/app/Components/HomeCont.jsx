602ca0628b4d1e527ca27e7b8d27bbaff79ac6bc
SatyarthBanerjee <satyarthbanerjee12@gmail.com>
2024-01-05 14:34:52 +0530

    first commit

---
+++ src/app/Components/HomeCont.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Homecont.module.css"
+const HomeCont = () => {
+  return (
+    <div className={styles.homecont}>
+      <h2>Task Manager</h2>
+    </div>
+  )
+}
+
+export default HomeCont
