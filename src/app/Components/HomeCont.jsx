e1c5f3adf3700a21c91e2a5ec4ded01616dddd15
SatyarthBanerjee <satyarthbanerjee12@gmail.com>
2024-01-06 13:47:01 +0530

    changes

--- src/app/Components/HomeCont.jsx
+++ src/app/Components/HomeCont.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import styles from "./Homecont.module.css"
+import React from "react";
+import styles from "./Homecont.module.css";
 const HomeCont = () => {
   return (
     <div className={styles.homecont}>
-      <h2>Task Manager</h2>
+      <h3>Task Manager</h3>
+      <div className={styles.labels}>
+        <p>Today</p>
+        <p>Tomorrow</p>
+        <p>Upcoming</p>
+      </div>
+      {/* <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="902"
+        height="3"
+        viewBox="0 0 902 3"
+        fill="none"
+      >
+        <path
+          d="M2 1.5H900"
+          stroke="#EDEDED"
+          stroke-width="3"
+          stroke-linecap="round"
+        />
+      </svg> */}
+      <div className={styles.line}>
+        <div className={styles.sublines}></div>
+        <div className={styles.sublines}></div>
+        <div className={styles.sublines}></div>
+       
+      </div>
+      <div className={styles.tasks}>
+        <div className={styles.today}>
+          <div className={styles.todaycont}>
+            <div className={styles.checkbox}></div>
+            <div className={styles.checkbox}></div>
+          </div>
+          <div className={styles.todaycont}></div>
+          <div className={styles.todaycont}></div>
+        </div>
+        <div className={styles.tomorrow}></div>
+        <div className={styles.upcoming}></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeCont
+export default HomeCont;
