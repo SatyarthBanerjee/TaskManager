f116d5915e1f209ac0e023c6f1ced291fa3b3510
SatyarthBanerjee <satyarthbanerjee12@gmail.com>
2024-01-07 00:37:28 +0530

    functions

--- src/app/Components/HomeCont.jsx
+++ src/app/Components/HomeCont.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import styles from "./Homecont.module.css";
+import { useState } from "react";
 const HomeCont = () => {
+  const [check, setCheck] = useState(false);
+  const [task, setTask] = useState(false);
+  const [todaydet, settodaydet] = useState({
+    task: "",
+    tag: "",
+    status: "",
+  });
+  const [details, setDetails] = useState([]);
+  const handleCheck = (id) => {
+    setDetails((prevDetails) => {
+      const updatedDetails = prevDetails.map((item, index) => {
+        if (index === id) {
+          return { ...item, status: !item.status };
+        }
+        return item;
+      });
+      return updatedDetails;
+    });
+  };
+  const handletask = () => {
+    setTask(!task);
+  };
+  const handleChange = (value, name) => {
+    settodaydet((prevDet) => {
+      return { ...prevDet, [name]: value };
+    });
+    console.log(todaydet);
+  };
+  const handleUpdate = () => {
+    setDetails((prevDetails) => [...prevDetails, todaydet]);
+  };
+  const [addtag, setaddtag] = useState(false);
+  const handleaddtag = () => {
+    setaddtag(!addtag);
+  };
+  useEffect(() => {
+    console.log(details);
+  }, [details]);
+  const handleKeyPress = (event) => {
+    // Check if the pressed key is Enter (key code 13)
+    if (event.key === "Enter") {
+      // Handle your logic here when Enter is pressed
+      handleUpdate()
+    }
+  };
   return (
-    <div className={styles.homecont}>
+    <div className={styles.homecont} onKeyPress={handleKeyPress}
+    tabIndex="0" >
       <h3>Task Manager</h3>
       <div className={styles.labels}>
         <p>Today</p>
@@ -30,21 +78,74 @@ const HomeCont = () => {
       </div>
       <div className={styles.tasks}>
         <div className={styles.today}>
-          <div className={styles.todaycont}>
-            <div className={styles.check}>
-              <div className={styles.checkbox}></div>
-            </div>
+          {details.map((item, id) => {
+            return (
+              <div className={styles.todaycont}>
+                <div className={styles.check}>
+                  <div
+                    onClick={() => handleCheck(id)}
+                    className={styles.checkbox}
+                    style={{
+                      backgroundColor: item.status === true ? "#FF3C58" : "",
+                    }}
+                  ></div>
+                </div>
 
-            <div className={styles.details}>
-              <p>Task Manager</p>
-              <div className={styles.tags}>
-                  <div className={styles.circle}></div>
-                  <p>Personal</p>
+                <div className={styles.details}>
+                  <p>{item.task}</p>
+                  <div className={styles.tags}>
+                    <div className={styles.circle}></div>
+                    <p>{item.tag}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
+
           {/* <div className={styles.todaycont}></div>
           <div className={styles.todaycont}></div> */}
+          {task && (
+            <div className={styles.inputtask}>
+              <div className={styles.inputtask_cont}>
+                <div className={styles.dottedcircle}></div>
+                <input
+                  value={todaydet.task}
+                  onChange={(e) => handleChange(e.target.value, "task")}
+                  placeholder="Add Task"
+                ></input>
+              </div>
+              <div className={styles.mytags}>
+              <div onClick={handleaddtag} className={styles.tags}>
+                <div className={styles.circle}></div>
+                <p>Add tag</p>
+              </div>
+              {addtag && (
+                <>
+                  <button
+                    className={styles.tags}
+                    value="personal"
+                    onClick={(e) => handleChange(e.target.value, "tag")}
+                  >
+                    Personal
+                  </button>
+                  <button
+                    value="work"
+                    className={styles.tags}
+                    onClick={(e) => handleChange(e.target.value, "tag")}
+                  >
+                    Work
+                  </button>
+                </>
+              )}
+              </div>
+             
+
+              {/* <button onClick={handleUpdate}>Update</button> */}
+            </div>
+          )}
+          <button className={styles.todayadd} onClick={handletask}>
+            + Add Task
+          </button>
         </div>
         <div className={styles.tomorrow}></div>
         <div className={styles.upcoming}></div>
