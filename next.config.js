d60787891330c11d26e5f1e68e638b35a8fac5aa
SatyarthBanerjee <satyarthbanerjee12@gmail.com>
2024-02-11 10:17:14 +0530

    dekhte h_1

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
       },
-      secret: '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',
+      // secret: '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',
 }
 
 module.exports = nextConfig
